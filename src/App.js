b17b33a9c35357badb659035de135935d0c02cfe
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-12-25 20:17:06 +0545

    implemented BrowserRouter, Route, Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
-  return <div>React Router V6</div>;
-}
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<div>Root</div>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
