e89ea6ebbfe8ed9baf36b503ff12e9eea420a926
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-12-25 20:26:53 +0545

    route components rendered with emotion css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { css } from '@emotion/css';
+
+import Products from './Products/Products';
+import Admin from './Admin/Admin';
+
+const AppStyles = css`
+  margin: 50px auto;
+  width: 380px;
+  .Container {
+    background: #1d1e26;
+    border: 4px solid #9580ff;
+    border-radius: 6px;
+    padding: 25px;
+  }
+`;
 
 const App = () => {
   return (
-    <div>
+    <div className={AppStyles}>
       <Router>
-        <Routes>
-          <Route path='/' element={<div>Root</div>} />
-        </Routes>
+        <div className='Container'>
+          <Routes>
+            <Route path='/' element={<Products />} />
+            <Route path='/admin' element={<Admin />} />
+          </Routes>
+        </div>
       </Router>
     </div>
   );
